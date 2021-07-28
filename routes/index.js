4baf78ef3d81b1ab6d3649761c5661cee603a1f9
Jaehoo Im <im.jaehoo1@gmail.com>
2021-07-29 01:24:32 +0900

    nodemailer test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const nodemailer = require('nodemailer');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const router = express.Router();
+
+router.post('/send', async (req, res, next) => {
+  console.log(req.body);
+  const { senderName, senderAddress, recipient, title, contentHtml } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.PORT,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.SMTP_USERNAME,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: `"${senderName}" <${senderAddress}>`, // sender address
+    to: recipient, // list of receivers 'a, b, c'
+    subject: title, // Subject line
+    text: 'Hello world?', // plain text body
+    html: contentHtml, // html body
+  });
+
+  console.log('Message sent: %s', info.messageId);
+
+  res.json({
+    result: 'ok',
+    message: `Message sent: ${info.messageId}`,
+  });
 });
 
 module.exports = router;
